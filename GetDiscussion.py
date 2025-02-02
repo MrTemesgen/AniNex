66b0af28aa50d96357afbc53111acb57e4f93061
Abreham Temesgen <tabreham2000@gmail.com>
2025-02-01 20:35:03 -0500

    Bug fix for episodes on multiples of 100 showing 100 episodes ahead.

--- GetDiscussion.py
+++ GetDiscussion.py
@@ -7,26 +7,38 @@ from dotenv import load_dotenv
 import os
 CLIENT_ID = os.getenv('CLIENT_ID')
 
+def getDiscussionBaseUrl(discussion_id):
+    return f"https://api.myanimelist.net/v2/forum/topic/{discussion_id}?&limit=100"
+
+# Get the discussion for the episode or anime not found message
 def get_discussion(anime, episode):
     current_app.logger.info(f"GET Discussion for {anime}-{episode}")
     anime_id = get_anime_id(anime)
     if anime_id == "": return jsonify(message = "Anime not found")
-    discussion_id = get_discussion_link(anime, anime_id, episode)
-    BASE_URL = f"https://api.myanimelist.net/v2/forum/topic/{discussion_id}?&limit=100"
-    discussion = requests.get(BASE_URL, headers = {'X-MAL-CLIENT-ID': f'{CLIENT_ID}'}).json()
+
+    discussion_id = get_discussion_link(anime, anime_id, episode) 
+    discussion = requests.get(getDiscussionBaseUrl(discussion_id), headers = {'X-MAL-CLIENT-ID': f'{CLIENT_ID}'}).json()
     data = discussion['data'] if 'data' in discussion else {}
     return jsonify(message = data)
 
+
+# Get the discussion link for the episode
 def get_discussion_link(anime, id, episode):
     try:
         episode = int(episode)
-        offset = (episode//100)*100 if episode > 100 else 0
+
+        # Each page has 100 episodes, so we need to offset the page by
+        # the greatest multiple of 100 less than the episode number
+        offset = ((episode-1)//100)*100 if episode > 100 else 0
         BASE_URL = f'https://myanimelist.net/anime/{id}/{anime}/episode?offset={offset}'
         
         response = requests.get(BASE_URL)
         soup = BeautifulSoup(response.content, 'html.parser')
         table = soup.find('table',  {'class': 'episode_list'})
+
         if table is None: return None
+
+        # Get the row of the episode
         remainder = (episode % 100)
         idx = 100 if remainder == 0 else remainder 
         row = table.find_all('tr')[idx]
